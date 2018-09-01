1b3c3f23b5471cfa47c4b52765a8ae0b62dba3f3
Yungoo Kong <ohsory1324@gmail.com>
2018-09-01 10:46:24 +0900

    add: 2-primitives

---
+++ 2-primitives/primitives.rs
@@ -0,0 +1,26 @@
+fn main() {
+  /*
+   *  Scalar Types
+   *    signed integers: i8, i16, i32, i64, i128 and isize (pointer size)
+   *    unsigned integers: u8, u16, u32, u64, u128 and usize (pointer size)
+   *    floating point: f32, f64
+   *    char Unicode scalar values like 'a', 'α' and '∞' (4 bytes each)
+   *    bool either true or false
+   *    and the unit type (), whose only possible value is an empty tuple: ()
+   *
+   *  Compound Types
+   *    arrays like [1, 2, 3]
+   *    tuples like (1, true)
+   */
+  let a_float: f64 = 1.0;  // Regular annotation
+  let an_integer   = 5i32; // Suffix annotation
+  let mut mutable = 12; // Mutable `i32`
+  mutable = 21;
+  // Error! The type of a variable can't be changed.
+  // mutable = true;
+  // Variables can be overwritten with shadowing.
+  let mutable = true;
+
+  println!("1 - 2 = {}", 1i32 - 2);
+  // println!("1 - 2 = {}", 1u32 - 2);
+}
